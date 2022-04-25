cf49cc0d0c464a3f2960ce8b0db411762a77d409
Benedict Renzon Alday <35050685+bralday@users.noreply.github.com>
2022-04-25 10:22:44 +0800

    Added pagination and sorting.
    - Added pagination and sorting for getAllPosts function.
    
    - Added AppConstants.java for storing constant variables
    
    - Added PostResponse.java for additional customization of pagination.

--- src/main/java/com/springboot/blog/service/impl/PostServiceImpl.java
+++ src/main/java/com/springboot/blog/service/impl/PostServiceImpl.java
@@ -3,9 +3,14 @@ package com.springboot.blog.service.impl;
 import com.springboot.blog.entity.Post;
 import com.springboot.blog.exception.ResourceNotFoundException;
 import com.springboot.blog.payload.PostDto;
+import com.springboot.blog.payload.PostResponse;
 import com.springboot.blog.repository.PostRepository;
 import com.springboot.blog.service.PostService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
@@ -38,9 +43,35 @@ public class PostServiceImpl implements PostService {
     }
 
     @Override
-    public List<PostDto> getAllPosts() {
-        List<Post> posts = postRepository.findAll();
-        return posts.stream().map(post -> mapToDto(post)).collect(Collectors.toList());
+    public PostResponse getAllPosts(int pageNo, int pageSize, String sortBy, String sortDir) {
+
+        // Determine whether asc or desc
+        Sort sort = sortDir.equalsIgnoreCase(Sort.Direction.ASC.name())
+                    ? Sort.by(sortBy).ascending()
+                    : Sort.by(sortBy).descending();
+
+        // Create pageable instance
+        // org.springframework.data.domain.Pageable
+        Pageable pageable = PageRequest.of(pageNo,pageSize, sort);
+
+        // Return a page of posts using pageable
+        Page<Post> posts = postRepository.findAll(pageable);
+
+        // Get content from page object
+        List<Post> listOfPost = posts.getContent();
+
+        List<PostDto> content =  listOfPost.stream().map(post -> mapToDto(post)).collect(Collectors.toList());
+
+        PostResponse postResponse = new PostResponse();
+        postResponse.setContent(content);
+        postResponse.setPageNo(posts.getNumber());
+        postResponse.setPageSize(posts.getSize());
+        postResponse.setTotalElements(posts.getTotalElements());
+        postResponse.setTotalPages(posts.getTotalPages());
+        postResponse.setLast(posts.isLast());
+
+        return postResponse;
+
     }
 
     @Override
